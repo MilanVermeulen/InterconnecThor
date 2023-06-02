74fbb2a24f9e9c42452fea5f1b47cfba7a71db27
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-06-02 14:37:41 +0200

    tiny edit to home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="row justify-content-center mt-5">
     <div class="col-md-10">
-        <h1 class="text-center">Hello, motherfuckers!</h1>
+        <h1 class="text-center mb-5">Hello, motherfuckers!</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt explicabo nihil libero dolor sit architecto quam tenetur temporibus debitis iusto nam assumenda facilis impedit voluptas, dolorum illo quibusdam magnam officia?</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt explicabo nihil libero dolor sit architecto quam tenetur temporibus debitis iusto nam assumenda facilis impedit voluptas, dolorum illo quibusdam magnam officia?</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt explicabo nihil libero dolor sit architecto quam tenetur temporibus debitis iusto nam assumenda facilis impedit voluptas, dolorum illo quibusdam magnam officia?</p>
