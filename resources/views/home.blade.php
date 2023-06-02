67700b2a7fbc066068f1de8307e33e2798034718
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-06-02 14:35:35 +0200

    added navbar + footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,12 @@
 @extends('master')
 
 @section('content')
-<div class="row">
+<div class="row justify-content-center mt-5">
     <div class="col-md-10">
-        <p>Hello World</p>
+        <h1 class="text-center">Hello, motherfuckers!</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt explicabo nihil libero dolor sit architecto quam tenetur temporibus debitis iusto nam assumenda facilis impedit voluptas, dolorum illo quibusdam magnam officia?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt explicabo nihil libero dolor sit architecto quam tenetur temporibus debitis iusto nam assumenda facilis impedit voluptas, dolorum illo quibusdam magnam officia?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt explicabo nihil libero dolor sit architecto quam tenetur temporibus debitis iusto nam assumenda facilis impedit voluptas, dolorum illo quibusdam magnam officia?</p>
     </div>
 </div>
 @endsection
