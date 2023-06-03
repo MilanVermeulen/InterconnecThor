31cb24a86330054e2c22b76da3cf1f76b8c417ae
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-06-03 23:21:41 +0200

    title/name change

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>InterconnecThor</title>
+	<title>iThor</title>
 	{{-- bootstrap css --}}
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 	{{-- custom css --}}
@@ -15,7 +15,7 @@
 	{{-- navbar --}}
 	<nav class="navbar navbar-expand-lg bg-body-tertiary" style="min-height: 10vh">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="/">InterconnectThor</a>
+			<a class="navbar-brand" href="/">iThor</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
